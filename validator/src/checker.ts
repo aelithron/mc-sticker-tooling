6943157c271da0d355cd68838a6f03318f0a7e5f
Nova <nova@novatea.dev>
2026-05-10 04:40:07 -0600

    validator: add slack id checking, improve error handling

--- validator/src/checker.ts
+++ validator/src/checker.ts
@@ -4,18 +4,40 @@ export default async function check(entry: Entry, caches: { airtable: DedupeCach
   const filteredCache = caches.airtable.filter((item) => entry.recordID !== item.recordID)
   if (filteredCache.find((item) => item.mcName === entry.mcName)) errors.push(`This record shares a Minecraft username with "${filteredCache.find((item) => item.mcName === entry.mcName)?.recordID}", likely duplicate!`);
   if (filteredCache.find((item) => item.slackID === entry.slackID)) errors.push(`This record shares a Slack ID with "${filteredCache.find((item) => item.slackID === entry.slackID)?.recordID}", likely duplicate!`);
-  if (!entry.mcName.startsWith(".")) {
-    let mojangAPI;
-    try {
-      mojangAPI = await fetch(`https://api.mojang.com/users/profiles/minecraft/${entry.mcName}`);
-    } catch (e) { throw new Error(`Error on Record ${entry.recordID} - Mojang API:\n${e}`); }
-    if (!mojangAPI.ok || (await mojangAPI.json()).errorMessage) errors.push(`Minecraft username "${entry.mcName}" doesn't exist!`);
-  } else {
-    let geyserAPI;
-    try {
-      geyserAPI = await fetch(`https://api.geysermc.org/v2/xbox/xuid/${entry.mcName.split(".")[1]}`);
-    } catch (e) { throw new Error(`Error on Record ${entry.recordID} - Geyser API:\n${e}`); }
-    if (!geyserAPI.ok || (await geyserAPI.json()).errorMessage) errors.push(`Minecraft username "${entry.mcName}" (Bedrock) doesn't exist!`);
+  if (entry.mcName) {
+    if (!entry.mcName.startsWith(".")) {
+      let mojangAPI;
+      try {
+        mojangAPI = await fetch(`https://api.mojang.com/users/profiles/minecraft/${entry.mcName}`);
+      } catch (e) { throw new Error(`Error on Record ${entry.recordID} - Mojang API:\n${e}`); }
+      const mojangBody = await mojangAPI.json();
+      if (!mojangAPI.ok || mojangBody.errorMessage) {
+        if ((mojangBody.errorMessage as string).includes("Couldn't find any profile with name")) {
+          errors.push(`Minecraft username "${entry.mcName}" doesn't exist!`);
+        } else throw new Error(`Error on Record ${entry.recordID} - Mojang API:\n${mojangBody.errorMessage}`);
+      }
+    } else {
+      let geyserAPI;
+      try {
+        geyserAPI = await fetch(`https://api.geysermc.org/v2/xbox/xuid/${entry.mcName.split(".")[1]}`);
+      } catch (e) { throw new Error(`Error on Record ${entry.recordID} - Geyser API:\n${e}`); }
+      const geyserBody = await geyserAPI.json()
+      if (!geyserAPI.ok) {
+        if ((geyserBody.message as string).includes("Unable to find user in our cache. Please try specifying their Floodgate UUID instead")) {
+          errors.push(`Minecraft username "${entry.mcName}" (Bedrock) doesn't exist!`);
+        } else throw new Error(`Error on Record ${entry.recordID} - Geyser API:\n${geyserBody.message}`); 
+      }
+    }
+  } else errors.push("This record is missing a Minecraft username!");
+  let slackAPI;
+  try {
+    slackAPI = await fetch(`https://slack.com/api/users.info?user=${entry.slackID}`, { headers: { "Authorization": `Bearer ${process.env.SLACK_BOT_TOKEN}` } });
+  } catch (e) { throw new Error(`Error on Record ${entry.recordID} - Slack API:\n${e}`); }
+  const slackBody = await slackAPI.json();
+  if (!slackAPI.ok || slackBody.error) {
+    if (slackBody.error === "user_not_found") {
+      errors.push(`Slack ID "${entry.slackID}" doesn't exist!`);
+    } else throw new Error(`Error on Record ${entry.recordID} - Slack API:\n${slackBody.error}`);
   }
   return { approved: (errors.length === 0), errors }
 }
