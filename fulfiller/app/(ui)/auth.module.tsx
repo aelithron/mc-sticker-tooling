1664eb5f00a80ff2cb6af224f150166268c81ae3
Nova <nova@novatea.dev>
2026-05-17 23:33:45 -0600

    fulfiller: bit more work

--- fulfiller/app/(ui)/auth.module.tsx
+++ fulfiller/app/(ui)/auth.module.tsx
@@ -1,11 +1,19 @@
 "use client"
 import { authClient } from "@/utils/auth";
+import { faDashboard, faSignIn, faSignOut } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 export default function AuthButton() {
   const session = authClient.useSession();
   if (!session.data) {
-    return <button onClick={async () => await authClient.signIn.social({ provider: "hca" })} className="flex flex-col text-center bg-violet-300 p-2 rounded-lg hover:text-sky-500">Sign In</button>
+    return <button onClick={async () => await authClient.signIn.social({ provider: "hca", callbackURL: "/dashboard" })} className="bg-violet-300 p-2 mt-2 rounded-lg hover:text-sky-500"><FontAwesomeIcon icon={faSignIn} /> Sign In</button>
   } else {
-    return <button onClick={async () => await authClient.signOut()} className="flex flex-col text-center bg-red-300 p-2 rounded-lg hover:text-sky-500">Sign Out</button>
+    return (
+      <div className="flex gap-2 mt-2 items-center text-center">
+        <Link href="/dashboard" className="bg-violet-300 p-2 rounded-lg hover:text-sky-500"><FontAwesomeIcon icon={faDashboard} /> Dashboard</Link>
+        <button onClick={async () => await authClient.signOut()} className="bg-red-300 p-2 rounded-lg hover:text-sky-500"><FontAwesomeIcon icon={faSignOut} /> Sign Out</button>
+      </div> 
+    );
   }
 }
