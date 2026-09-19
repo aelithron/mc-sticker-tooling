e866d624d1511f0aa3703ce6ecea117c4170391e
Nova <nova@novatea.dev>
2026-09-19 16:57:10 -0700

    panel: add comments and rejecting requests

--- panel/app/validator/page.tsx
+++ panel/app/validator/page.tsx
@@ -1,7 +1,7 @@
 import loadTable from "@/utils/airtable";
 import { auth } from "@/utils/auth";
 import loadConfig from "@/utils/config";
-import { faArrowLeft, faCheck, faDatabase, faEnvelope, faFlag, faHome, faMessage, faRefresh } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft, faCheck, faClock, faDatabase, faEnvelope, faFlag, faHome, faMessage, faRefresh } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Metadata } from "next";
 import { headers } from "next/headers";
@@ -32,7 +32,7 @@ export default async function Page() {
       <h1 className="font-semibold text-3xl"><FontAwesomeIcon icon={faCheck} /> Validator</h1>
       <div className="flex gap-2">
         {!config.runningValidation ? <ValidateLetters /> : <p className="bg-violet-300 text-violet-500 py-1 px-2 gap-1 rounded-xl flex items-center w-fit"><FontAwesomeIcon icon={faRefresh} /> Validation running...</p>}
-        <a href={`https://airtable.com/${process.env.AIRTABLE_BASE_ID}/${process.env.AIRTABLE_TABLE_ID}`} target="_blank" className="bg-violet-400 py-1 px-2 rounded-xl flex w-fit items-center gap-1"><FontAwesomeIcon icon={faDatabase} /> Open Airtable</a>
+        <a href={`https://airtable.com/${process.env.AIRTABLE_BASE_ID}/${process.env.AIRTABLE_TABLE_ID}`} target="_blank" className="bg-violet-400 py-1 px-2 rounded-xl flex w-fit items-center gap-1 hover:text-sky-500"><FontAwesomeIcon icon={faDatabase} /> Open Airtable</a>
       </div>
       <h1 className="text-2xl font-semibold mt-4"><FontAwesomeIcon icon={faEnvelope} /> Pending Requests</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
@@ -49,10 +49,12 @@ export default async function Page() {
 }
 
 async function ValidatorCard({ letter }: { letter: Letter }) {
-  let comments: string[];
+  const comments: { id: string, author: string, text: string, createdAt: Date }[] = [];
   try {
-    const res = await fetch(`https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${process.env.AIRTABLE_TABLE_ID}/${letter.recordID}/comments`);
-    comments = await res.json();
+    const res = await fetch(`https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${process.env.AIRTABLE_TABLE_ID}/${letter.recordID}/comments`, { headers: { "Authorization": `Bearer ${process.env.AIRTABLE_API_KEY}` } });
+    const commentData = await res.json();
+    if (!commentData.comments) console.error(`Failed to load comments: ${commentData.error.message} (${commentData.error.type})`);
+    for (const comment of (commentData.comments || [])) comments.push({ id: comment.id as string, author: comment.author.name as string, text: comment.text as string, createdAt: new Date(comment.createdTime as string) });
   } catch (e) {
     console.error(`Failed to load comments: ${e}`);
   }
@@ -60,12 +62,20 @@ async function ValidatorCard({ letter }: { letter: Letter }) {
     <div key={letter.recordID} className="flex flex-col p-2 bg-violet-300 rounded-lg">
       <h2 className="font-semibold">{letter.mcName}</h2>
       <div className="grid grid-cols-2 grid-rows-1 gap-2 mt-2 text-sm">
-        <a href={`https://hackclub.slack.com/team/${letter.slackID}`} target="_blank" className="bg-violet-400 p-2 rounded-xl"><FontAwesomeIcon icon={faMessage} /> Message</a>
-        <a href={`https://airtable.com/${process.env.AIRTABLE_BASE_ID}/${process.env.AIRTABLE_TABLE_ID}/${letter.recordID}?blocks=hide`} target="_blank" className="bg-violet-400 p-2 rounded-xl"><FontAwesomeIcon icon={faDatabase} /> Open in Airtable</a>
+        <a href={`https://hackclub.slack.com/team/${letter.slackID}`} target="_blank" className="bg-violet-400 p-2 rounded-xl hover:text-sky-500"><FontAwesomeIcon icon={faMessage} /> Message</a>
+        <a href={`https://airtable.com/${process.env.AIRTABLE_BASE_ID}/${process.env.AIRTABLE_TABLE_ID}/${letter.recordID}?blocks=hide`} target="_blank" className="bg-violet-400 p-2 rounded-xl  hover:text-sky-500"><FontAwesomeIcon icon={faDatabase} /> Open in Airtable</a>    
       </div>
       <div className="flex gap-2 text-sm mt-3 items-center">
         <p>Override Status:</p>
         <OverrideStatus id={letter.recordID} />
+      </div>
+      <div className="flex flex-col mt-2 gap-2">
+        <h1>Comments ({comments.length})</h1>
+        {comments.sort((a, b) => { return (a.createdAt.getTime() - b.createdAt.getTime()); }).map((comment) => <div key={comment.id} className="flex flex-col gap-2 bg-violet-400/50 p-2 rounded-lg">
+          <h2 className="flex gap-1 items-center">{comment.author} <p className="text-sm p-1 bg-violet-500/50 rounded-xl"><FontAwesomeIcon icon={faClock} /> {comment.createdAt.toLocaleString("en-us")}</p></h2>
+          <p className="whitespace-pre-wrap">{comment.text}</p>
+        </div>)}
+        {comments.length < 1 && <p>There are no comments on this!</p>}
       </div>
     </div>
   );
--- panel/app/validator/validation.module.tsx
+++ panel/app/validator/validation.module.tsx
@@ -31,8 +31,9 @@ export function OverrideStatus({ id }: { id: string }) {
       <select value={approval} className="bg-violet-400 p-1 rounded-xl" onChange={(e) => setApproval(e.target.value)}>
         <option value={""}> -- Select -- </option>
         <option value={"Pending"}>Pending</option>
-        <option value={"Flagged"}>Flagged</option>
         <option value={"Approved"}>Approved</option>
+        <option value={"Flagged"}>Flagged</option>
+        <option value={"Rejected"}>Rejected</option>
       </select>
       <button type="submit" className="bg-violet-400 p-1 rounded-xl"><FontAwesomeIcon icon={faCaretRight} /></button>
     </form>
@@ -56,5 +57,5 @@ export function ValidateLetters() {
       return;
     }
   }
-  return <button onClick={handleValidate} className="bg-violet-400 py-1 px-2 gap-1 rounded-xl flex items-center w-fit"><FontAwesomeIcon icon={faCheck} /> Validate Pending</button>
+  return <button onClick={handleValidate} className="bg-violet-400 py-1 px-2 gap-1 rounded-xl flex items-center w-fit hover:text-sky-500"><FontAwesomeIcon icon={faCheck} /> Validate Pending</button>
 }
